7886b821eb4434f32ea3b2a85afd3a06da140e59
Lucian Murdin <lpmurdin1@sheffield.ac.uk>
2022-07-17 22:29:57 +0100

    added ejs files and todo for server

---
+++ public/javascripts/index.js
@@ -0,0 +1,10 @@
+
+import { Octokit, App } from "octokit";
+
+function init() {
+    const octokit = new Octokit({})
+}
+
+function submitUsername() {
+
+}
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Github Stats' });
 });
 
+/* @todo make a POST that supplies the authentication for the Github API */
+
+/* POST on repos */
+router.post('/repos', function(req, res, next) {
+  /* @todo get token, retrieve JSON and fill repos */
+  res.render('repos', { title: 'Github Stats' });
+});
+
 module.exports = router;
